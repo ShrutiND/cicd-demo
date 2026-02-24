a617914e6a0e431d09a87cf44597d2ed85bcbb55
ShrutiDongre08 <shrutidongre08@gmail.com>
2026-02-24 17:01:40 +0530

    Initial CI CD project

---
+++ src/main/java/com/shruti/cicd_demo/CicdDemoApplication.java
@@ -0,0 +1,15 @@
+package com.shruti.cicd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CicdDemoApplication.class, args);
+
+	}
+
+}
